3059d3e41a216a3f12abb3eee4cebd5f258645e5
Faizir Rohman <55338058+faizirrohman@users.noreply.github.com>
2022-10-10 14:33:28 +0700

    add handling error di PrintController

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -10,8 +10,14 @@ class PrintController extends Controller
 {
     public function print($id)
     {
-        $daftar = Daftar::findorFail($id);
-        $pdf = PDF::loadview('daftar.print', compact('daftar'))->setPaper('A4','portrait');
-        return $pdf->stream();
+        try {
+            $daftar = Daftar::findorFail($id);
+            $pdf = PDF::loadview('daftar.print', compact('daftar'))->setPaper('A4','portrait');
+            
+            return $pdf->stream();
+        } catch (\Exception $e) {
+            \Log::error($e);
+            return "Terjadi kesalahan";
+        }
     }
 }
